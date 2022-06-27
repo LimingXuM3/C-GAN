30e41ca091ab47bee57b80a7aced483078a9cbe8
Bryan Xu <xulimmail@gmail.com>
2022-06-27 11:59:57 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,218 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class BasicEncoder(nn.Module):
+    add_image = False
+
+    def _conv2d(self, in_channels, out_channels):
+        return nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            padding=1
+        )
+
+    def _build_models(self):
+        self.features = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.layers = nn.Sequential(
+            self._conv2d(self.hidden_size + self.data_depth, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+            self._conv2d(self.hidden_size, 3),
+            nn.Tanh(),
+        )
+        return self.features, self.layers
+
+    def __init__(self, data_depth, hidden_size):
+        super().__init__()
+        self.version = '1'
+        self.data_depth = data_depth
+        self.hidden_size = hidden_size
+        self._models = self._build_models()
+
+    def upgrade_legacy(self):
+        if not hasattr(self, 'version'):
+            self.version = '1'
+
+    def forward(self, image, data):
+        x = self._models[0](image)
+        x_list = [x]
+
+        for layer in self._models[1:]:
+            x = layer(torch.cat(x_list + [data], dim=1))
+            x_list.append(x)
+
+        if self.add_image:
+            x = image + x
+
+        return x
+
+class ResidualEncoder(BasicEncoder):
+    add_image = True
+
+    def _build_models(self):
+        self.features = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.layers = nn.Sequential(
+            self._conv2d(self.hidden_size + self.data_depth, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+            self._conv2d(self.hidden_size, 3),
+        )
+        return self.features, self.layers
+
+class DenseEncoder(BasicEncoder):
+    add_image = True
+
+    def _build_models(self):
+        self.conv1 = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv2 = nn.Sequential(
+            self._conv2d(self.hidden_size + self.data_depth, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv3 = nn.Sequential(
+            self._conv2d(self.hidden_size * 2 + self.data_depth, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+        )
+        self.conv4 = nn.Sequential(
+            self._conv2d(self.hidden_size * 3 + self.data_depth, 3)
+        )
+
+        return self.conv1, self.conv2, self.conv3, self.conv4
+    
+class BasicCritic(nn.Module):
+    def __init__(self):
+        super(BasicCritic, self).__init__()
+        self.ngpu = 1
+        self.main = nn.Sequential(
+            # input is (nc) x 32 x 32
+            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 16 x 16
+            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 8 x 8
+            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 4 x 4
+            nn.Conv2d(64 * 4, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+
+        return output.view(-1, 1).squeeze(1)
+
+class BasicDecoder(nn.Module):
+    def _conv2d(self, in_channels, out_channels):
+        return nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            padding=1
+        )
+
+    def _build_models(self):
+        self.layers = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size),
+
+            self._conv2d(self.hidden_size, self.data_depth)
+        )
+
+        return [self.layers]
+
+    def __init__(self, data_depth, hidden_size):
+        super().__init__()
+        self.version = '1'
+        self.data_depth = data_depth
+        self.hidden_size = hidden_size
+
+        self._models = self._build_models()
+
+    def upgrade_legacy(self):
+        if not hasattr(self, 'version'):
+            self._models = [self.layers]
+
+            self.version = '1'
+
+    def forward(self, x):
+        x = self._models[0](x)
+
+        if len(self._models) > 1:
+            x_list = [x]
+            for layer in self._models[1:]:
+                x = layer(torch.cat(x_list, dim=1))
+                x_list.append(x)
+
+        return x
+
+class DenseDecoder(BasicDecoder):
+    def _build_models(self):
+        self.conv1 = nn.Sequential(
+            self._conv2d(3, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size)
+        )
+
+        self.conv2 = nn.Sequential(
+            self._conv2d(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size)
+        )
+
+        self.conv3 = nn.Sequential(
+            self._conv2d(self.hidden_size * 2, self.hidden_size),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(self.hidden_size)
+        )
+
+        self.conv4 = nn.Sequential(self._conv2d(self.hidden_size * 3, self.data_depth))
+
+        return self.conv1, self.conv2, self.conv3, self.conv4
+
+    def upgrade_legacy(self):
+        if not hasattr(self, 'version'):
+            self._models = [
+                self.conv1,
+                self.conv2,
+                self.conv3,
+                self.conv4
+            ]
+
+            self.version = '1'
